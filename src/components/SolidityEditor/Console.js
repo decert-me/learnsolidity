4d1be2b814515618cfb23e85795ef23b7febd366
Caoqizhi <2391580279@qq.com>
2023-04-06 16:18:54 +0800

    en: 添加滚动条设置

--- src/components/SolidityEditor/Console.js
+++ src/components/SolidityEditor/Console.js
@@ -1,7 +1,16 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 export default function Console(props) {
     const { log } = props;
+
+    function scrollTopChange() {
+        var element = document.querySelector(".console");
+        element.scrollTop = element.scrollHeight;
+    }
+
+    useEffect(() => {
+        scrollTopChange()
+    },[log])
 
     return (
         log.length > 0 &&
